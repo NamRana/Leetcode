d65acc2d92d3272a6141206e99fa0a89acc521af
Naman Rana <91771208+NamRana@users.noreply.github.com>
2025-09-30 01:38:11 +0530

    Time: 0 ms (100%), Space: 11.1 MB (90.01%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -1,21 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int minScoreTriangulation(vector<int>& values) {
-        int n=values.size();
+        int n = values.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        vector<vector<int>> dp(n,vector<int>(n,0));
-
-        for(int len=3;len<=n;len++){
-            for(int i=0;i+len-1<n;i++){
-                int j=i+len-1;
-                dp[i][j]=INT_MAX;
-
-                for(int k=i+1;k<j;k++){
-                    int score=dp[i][k]+dp[k][j]+values[i]*values[k]*values[j];
-                    dp[i][j]=min(dp[i][j],score);
+        // length of interval (at least 3 to form a triangle)
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j],
+                                   dp[i][k] + dp[k][j] + values[i] * values[k] * values[j]);
                 }
             }
         }
-        return dp[0][n-1];
+        return dp[0][n - 1];
     }
 };
