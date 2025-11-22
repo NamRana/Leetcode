6444a4cb778615449e584ed38cc069a48d787f01
Naman Rana <91771208+NamRana@users.noreply.github.com>
2025-11-22 18:03:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.19%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     int solve(int n, vector<int>& memo) {
+        if (n == 1 || n == 2) return n;
+
+        // If already computed → return stored value
+        if (memo[n] != -1) return memo[n];
+
+        // Store & return result
+        memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return solve(n, memo);
+    }
+};
