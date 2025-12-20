68ac4ff3d4c259c8e16f33168653b9d4e03923ef
Naman Rana <91771208+NamRana@users.noreply.github.com>
2025-12-20 23:30:57 +0530

    Time: 0 ms (100%), Space: 16.8 MB (73.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,45 +1,54 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
+
         queue<pair<int,int>> q;
         int fresh=0;
 
-        //push all rotten oranges into queue
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
-                }else if(grid[i][j]==1)fresh++;
-            }
-        }
-
-        //if no fresh oranges at start
-        if(fresh==0)return 0;
-
-        int minutes=-1;//will become 0 after first level
-        int dirs[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
-
-        //BFS
-        while(!q.empty()){
-            int size=q.size();
-            minutes++;
-
-            while(size--){
-                auto [x,y]=q.front();
-                q.pop();
-
-                for(auto& d: dirs){
-                    int nx=x+d[0],ny=y+d[1];
-
-                    if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]==1){
-                        grid[nx][ny]=2;
-                        fresh--;
-                        q.push({nx,ny});
-                    }
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-        return (fresh==0)?minutes : -1;
+
+        if(fresh==0)return 0;
+        int min=0;
+
+        //BFS traversal
+        while(!q.empty()){
+            int size=q.size();
+            bool rotminute=false;
+
+            //process one BFS level
+            for(int i=0;i<size;i++){
+                auto[x,y]=q.front();
+                q.pop();
+
+                for(int d=0;d<4;d++){
+                    int nx=x+dx[d];
+                    int ny=y+dy[d];
+
+                    if(nx>=0&&ny>=0&&nx<rows&&ny<cols&&grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        fresh--;
+                        q.push({nx,ny});
+                        rotminute=true;
+                    }
+                }
+            }
+            if(rotminute)min++;
+        }
+        return fresh==0? min:-1;
+
     }
 };
