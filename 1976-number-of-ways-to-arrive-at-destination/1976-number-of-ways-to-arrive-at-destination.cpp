579e500bb1578a5e97c356cd570aa5e6efb00c3f
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-01-24 22:06:57 +0530

    Time: 8 ms (56.72%), Space: 35.2 MB (65.44%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        const long long INF = 1e18;
         const int MOD = 1e9 + 7;
-
-        // adjacency list: node -> {neighbor, time}
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto &r : roads) {
-            adj[r[0]].push_back({r[1], r[2]});
-            adj[r[1]].push_back({r[0], r[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &r: roads){
+            adj[r[0]].push_back({r[1],r[2]});
+            adj[r[1]].push_back({r[0],r[2]});
         }
 
-        vector<long long> dist(n, INF);
-        vector<long long> ways(n, 0);
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> ways(n,0);
+        dist[0]=0;
+        ways[0]=1;
 
-        priority_queue<
-            pair<long long, int>,
-            vector<pair<long long, int>>,
-            greater<>
-        > pq;
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<>> pq;
 
-        dist[0] = 0;
-        ways[0] = 1;
-        pq.push({0, 0});
+        pq.push({0,0});
 
-        while (!pq.empty()) {
-            auto [time, u] = pq.top();
+        while(!pq.empty()){
+            auto [time,u]=pq.top();
             pq.pop();
 
-            if (time > dist[u]) continue;
+            if(time>dist[u])continue;
 
-            for (auto &[v, w] : adj[u]) {
-                long long newTime = time + w;
+            for(auto &[v,w]:adj[u]){
+                long long newTime=time+w;
 
-                if (newTime < dist[v]) {
-                    dist[v] = newTime;
-                    ways[v] = ways[u];
-                    pq.push({newTime, v});
+                if(newTime<dist[v]){
+                    dist[v]=newTime;
+                    ways[v]=ways[u];
+                    pq.push({newTime,v});
                 }
-                else if (newTime == dist[v]) {
-                    ways[v] = (ways[v] + ways[u]) % MOD;
+                else if(newTime==dist[v]){
+                    ways[v]=(ways[v]+ways[u])%MOD;
                 }
             }
         }
-
-        return ways[n - 1];
+        return ways[n-1];
     }
 };
