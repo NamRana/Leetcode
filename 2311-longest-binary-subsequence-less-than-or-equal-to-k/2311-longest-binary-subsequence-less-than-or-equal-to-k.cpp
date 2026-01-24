bf1ec372dc21ca10911c53d4d1134cb6622c08dd
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-01-24 12:47:20 +0530

    Time: 0 ms (100%), Space: 9.6 MB (34.66%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int value = 0;
-        int power = 1;
-        int count = 0;
-        
-        // Traverse from right (LSB) to left
-        for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '0') {
-                // zero is always safe
-                count++;
-            } else {
-                // try including '1'
-                if (value + power <= k) {
-                    value += power;
+        int value=0;
+        int power=1;
+        int count=0;
+
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='0')count++;
+            else{
+                if(value+power<=k){
+                    value+=power;
                     count++;
                 }
             }
-            
-            // prevent overflow & useless large powers
-            if (power <= k)
-                power <<= 1;
+            if(power<=k){
+                power<<=1;
+            }
         }
-        
         return count;
     }
 };
