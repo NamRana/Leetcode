a0e18e95a8b235ed56e816a005fc1c0fadd4f18c
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-01-26 04:03:55 +0530

    Time: 0 ms (100%), Space: 9.6 MB (47.43%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int maxCount=1;
+        int currentCount=1;
+
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1]){
+                currentCount++;
+            }else{
+                currentCount=1;
+            }
+
+            maxCount=max(maxCount,currentCount);
+        }
+    return maxCount;
+    }
+};
