e248aa154c4b6ad89d30420133e66b64d242d715
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-01-26 23:43:30 +0530

    Time: 0 ms (100%), Space: 9.8 MB (39.85%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
@@ -5,10 +5,10 @@ public:
 
         for(int i=0;i<s.size();i+=k){
             string group=s.substr(i,k);
-
-            if(group.length()<k){
-                group+=string(k-group.length(),fill);
+            if(group.size()<k){
+                group+=string(k-group.size(),fill);
             }
+
             ans.push_back(group);
         }
         return ans;
