dd8f003e7acf0588c92ec468074c78f51fb9d45b
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-01-28 03:23:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (58.87%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string word;
+        int index=1;
+
+        while(ss>>word){
+            if(word.size()>=searchWord.size()&&word.substr(0,searchWord.size())==searchWord){
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+};
