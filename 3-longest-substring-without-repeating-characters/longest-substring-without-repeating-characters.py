523e052ddd2b058a620697ef36336c11788b128a
Naman Rana <91771208+NamRana@users.noreply.github.com>
2026-04-18 23:18:18 +0530

    Time: 13 ms (50.42%) | Memory: 19.2 MB (74.54%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,6 +1,9 @@
-from functools import reduce
-
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return reduce(lambda acc, ic: (max(acc[0], ic[0] - max(acc[1], acc[2].get(ic[1], -1) + 1) + 1), max(acc[1], acc[2].get(ic[1], -1) + 1), {**acc[2], ic[1]: ic[0]}), enumerate(s), (0, 0, {}))[0]
+        seen,left,ans={},0,0
+        for i,c in enumerate(s):
+            if c in seen:
+                left=max(left,seen[c]+1)
+            seen[c]=i
+            ans=max(ans,i-left+1)
+        return ans
